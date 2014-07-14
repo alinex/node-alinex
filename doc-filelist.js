d24c7b359c5206fcea0511017f72dca6a7c1322a
alinex <info@alinex.de>
2014-07-14 09:04:38 +0200

    Updated documentation

--- doc-filelist.js
+++ doc-filelist.js
@@ -1,1 +1,1 @@
-var tree={"files":["Changelog.md","README.md"],"dirs":{"src":{"dirs":{"doc":{"files":["3rdparty.md","coffee.md","developtools.md","filestructure.md","git.md","install.md","npm.md","styleguide.md","sublime.md"]}}}}};
+var tree={"files":["Changelog.md","README.md"],"dirs":{"src":{"dirs":{"doc":{"files":["3rdparty.md","atom.md","coffee.md","developtools.md","filestructure.md","git.md","ideas.md","install.md","javascript.md","npm.md","styleguide.md","sublime.md"]}}}}};
